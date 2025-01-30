fa8613cf0079d7d3a7a87e3c4dc5daaebd3f4fd5
DelTa-0 <aryalmadhave123@gmail.com>
2025-01-30 20:15:44 +0545

    django admin work adding product list in admin panel using models file

--- djangoproject/djangoapp/models.py
+++ djangoproject/djangoapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Products(models.Model):
+    product_type=[
+        ('watch','rolex'),
+        ('phone','samsung'),
+        ('apple','iphone'),
+        ('laptop','acer'),
+    ]
+    name=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='pictures/')
+    date_added= models.DateTimeField(default=timezone.now)
+    type= models.CharField(max_length=6,choices=product_type)
+
+    def __str__(self):
+        return self.name
+    
