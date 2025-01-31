691d4ecd33cd8cad21b6638ca73caf178b40345a
DelTa-0 <aryalmadhave123@gmail.com>
2025-01-31 17:16:05 +0545

    admin model configuration

--- djangoproject/djangoapp/admin.py
+++ djangoproject/djangoapp/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products,productCertificate,productReview,Store
+
+class productReviewInline(admin.TabularInline):
+    model=productReview
+    extra=2
+
+class productsAdmin(admin.ModelAdmin):
+    list_display=('name','type','date_added')
+    inlines=[productReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display=('name','location')
+    filter_horizontal=('products',)
+
+class ProductCertificateAdmin(admin.ModelAdmin):
+    list_display=('product','certificate_number')
+
+
 # Register your models here.
-admin.site.register(Products)
+admin.site.register(Products,productsAdmin)
+admin.site.register(Store,StoreAdmin)
+admin.site.register(productCertificate,ProductCertificateAdmin)
--- djangoproject/djangoapp/models.py
+++ djangoproject/djangoapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Products(models.Model):
@@ -18,3 +19,32 @@ class Products(models.Model):
     def __str__(self):
         return self.name
     
+#one to many
+class productReview(models.Model):
+    product=models.ForeignKey(Products,on_delete=models.CASCADE,related_name='reviews')
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    rating=models.IntegerField()
+    comment=models.TextField()
+    date_added=models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.product.name}'
+    
+#many to many
+class Store(models.Model):
+    name=models.CharField(max_length=100)
+    location=models.CharField(max_length=50)
+    products=models.ManyToManyField(Products,related_name='stores')
+
+    def __str__(self):
+        return self.name
+    
+#one to one
+class productCertificate(models.Model):
+    product=models.OneToOneField(Products,on_delete=models.CASCADE,related_name='certificate')
+    certificate_number=models.CharField(max_length=100)
+    issue_date=models.DateTimeField(default=timezone.now)
+    valid_date=models.DateTimeField()
+
+    def __str__(self):
+        return f'certificate for {self.name.product}'
